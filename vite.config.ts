76fad42a3ac7b9fa8806857e9dd4edf6b6b3efa8
akukalakaka <98338152+W1zar707@users.noreply.github.com>
2026-09-22 12:55:58 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/assets/',
+  base: '/siteHost/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
